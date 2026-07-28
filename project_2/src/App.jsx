02c417f93da8cb745d0c3760dcb7ceb759387603
UttamUpadhyay <upadhyayuttam197@gmail.com>
2026-07-28 20:10:30 +0530

    Project_2

---
+++ project_2/src/App.jsx
@@ -0,0 +1,19 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
+// import './App.css'
+import Welcome from "./welcome"
+
+function App() {
+  return (
+  <div>
+    <h1>
+      Main App Component
+    </h1>
+    <Welcome/>
+  </div>
+  );
+}
+
+export default App;
---
+++ project_2/src/welcome.jsx
@@ -0,0 +1,9 @@
+function Welcome() {
+    return (
+        <div>
+            <h1>Welcome to the Project!</h1>
+            <p>This is the welcome page of our application.</p>
+        </div>
+    );
+}
+export default Welcome;
