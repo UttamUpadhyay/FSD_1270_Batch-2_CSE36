77f05f3f548c41501c3afd56a5540efa35f172f4
UttamUpadhyay <upadhyayuttam197@gmail.com>
2026-09-01 11:51:38 +0530

    file_create

---
+++ express-demo/fcreate.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+fs.writeFile("student.txt", "Hello Students!", (err) => {
+    if (err) {
+        console.log("Error creating file : ", err);
+        return;
+    }
+    console.log("File created Successfully!");
+});
